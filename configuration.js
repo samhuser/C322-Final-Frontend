cdfd267db5de3fd9f7c8aa6c27aa18bc0c96e488
coldapollo8 <63075497+coldapollo8@users.noreply.github.com>
2024-05-02 05:08:51 -0400

    submission

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
-const mode = 0;
+const mode = 1;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://ducks-service-m9bg.onrender.com";
+const host_remote = "https://flowers-latest.onrender.com";
 
 function getHost() {
   return mode == 0 ? host_local : host_remote;
